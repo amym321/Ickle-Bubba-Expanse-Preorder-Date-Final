d0b0f7100d2fa6f0e24bb50b1e1837f02aabe4b9
Amy Mick <amymick847@gmail.com>
2023-07-25 13:48:07 -0500

    first commit

---
+++ assets/gem-page-111957606684.js
@@ -0,0 +1,68 @@
+
+
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    (function( jQuery ){
+  // var $module = jQuery('#m-1677772435873').children('.module');
+  // You can add custom Javascript code right here.
+})( window.GemQuery || jQuery );
+  
+    (function( jQuery ){
+  // var $module = jQuery('#m-1677772495981').children('.module');
+  // You can add custom Javascript code right here.
+})( window.GemQuery || jQuery );
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
---
+++ assets/gem-page-112178725148.js
@@ -0,0 +1,31 @@
+
+
+    
+  
+    
+  
+    
+  (function( jQuery ){
+  // var $module = jQuery('#m-1684848417450').children('.module');
+  // You can add custom Javascript code right here.
+console.log("before clicking button");
+
+$(".distributor-form").click(function(e){
+  exponea.showBanner('6479e69fa40332d1bc4870d8');
+  console.log("after clicking button");
+});
+})( window.GemQuery || jQuery );
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
